322a8bad55aed3b7f9a742dba54fe7f85a2a1f30
Dungnguyen107 <nguyentiendung71005@gmail.com>
2026-04-30 17:34:04 +0700

    lesson4:arithmetic operators

---
+++ ls4_arithmetic_operators.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 10;
+    int y = 3;
+    int z = 0;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
+    // x++; // x=x+1, x+=1
+    // x--; // x=x-1,x-=1
+
+    // augmented assignment operators
+
+    // x+=3
+    // x-=3
+    // x*=3
+    x/=2;
+
+    printf("%d", x);
+
+    // when using divde operator /, the dividing number would be less risk when being float
+    // the result of int /int cannot be a int, so the program cannot store the value
+
+    return 0;
+}
