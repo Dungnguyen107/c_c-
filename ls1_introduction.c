772619e698be5c1c062484074b2da2e501f6be9e
Dungnguyen107 <nguyentiendung71005@gmail.com>
2026-04-30 14:08:04 +0700

    lesson1_introduction

---
+++ ls1_introduction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // This is how to make a comment in C language.
+    /*
+    This is how to make many lines of comment in C language
+    See, it's quite simpple. Right?
+    Just take all your comment between "/*" and it reverse position
+    */
+    printf("I like pizza!\n");
+    printf("It's really good!");
+
+
+    return 0;
+}
